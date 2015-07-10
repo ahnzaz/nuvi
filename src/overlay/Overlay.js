0b7d7d3a4bc3b1d9a46d44b808202a00d6dc0a44
ahnzaz <ahnzaz@gmail.com>
2015-07-11 01:13:22 +0900

    Update Overlay.js

--- src/overlay/Overlay.js
+++ src/overlay/Overlay.js
@@ -1,1 +1,21 @@
-function(){}
+/**
+ * Overlay entity class. <br />
+ * @since 1.0
+ *
+ * @constructor
+ */
+(function (_window) {
+    function OverlayClass(_obj) {
+        this.center     = _obj.center;
+        this.element    = _obj.element;
+        this.container  = null;
+    }
+
+    OverlayClass.prototype.show = function () {
+        this.constainer.display = 'none';
+    };
+
+    OverlayClass.prototype.hide = function(){
+        this.container.display  = 'box';
+    }
+}(window)());
